3964a2e51afed9817d35173fa9e5664625a784db
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-29 12:43:20 -0300

    fix: update tests

--- test/cubit/counter_cubit_test.dart
+++ test/cubit/counter_cubit_test.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_hero/cubit/counter_cubit.dart';
+import 'package:bloc_hero/logic/cubit/counter_cubit.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
